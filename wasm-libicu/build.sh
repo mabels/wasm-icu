0793517ee167767d3086d4ebe609380a3f365c29
Meno Abels <meno.abels@adviser.com>
2019-08-28 16:23:06 +0200

    chore: fix build to run wasm32

--- wasm-libicu/build.sh
+++ wasm-libicu/build.sh
@@ -1,8 +1,9 @@
-CFLAGS="-s TOTAL_MEMORY=32MB" \
-CPPFLAGS="-s TOTAL_MEMORY=32MB" \
-CXXFLAGS="-s TOTAL_MEMORY=32MB -s WASM=1 -s FORCE_FILESYSTEM=1 -s NODERAWFS=1 -std=c++11" \
-emconfigure ./configure --enable-static --prefix=/src/dist
-sed -i.orig 's/^EXEEXT =/EXEEXT = .js/' icudefs.mk
+#CFLAGS="-s TOTAL_MEMORY=32MB" \
+#CPPFLAGS="-s TOTAL_MEMORY=32MB" \
+#CXXFLAGS="-s TOTAL_MEMORY=32MB -s WASM=1 -s FORCE_FILESYSTEM=1 -s NODERAWFS=1 -std=c++11" \
+#emconfigure ./configure --enable-static --prefix=/src/dist
+#sed -i.orig 's/^EXEEXT =/EXEEXT = .js/' icudefs.mk
+./runConfigureICU wasm32 --prefix=/src/dist
 make clean
 make -j$(nproc)
 make -j$(nproc) install
