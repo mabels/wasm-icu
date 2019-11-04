bc9406effe157a0b929a323ea6e5c333a918e3b8
Meno Abels <meno.abels@adviser.com>
2019-11-04 10:54:36 +0100

    chore: switch local emcc or docker emcc

--- wasm-libicu/container.sh
+++ wasm-libicu/container.sh
@@ -2,8 +2,13 @@
 
 sh -x prepare.sh $BRANCH
 
-docker rmi trzeci/emscripten
-docker run --rm -v `pwd`:/src trzeci/emscripten \
-  bash -c "(cd icu/icu4c/source && bash -x ../../../build.sh)"
+buildCmd="cd icu/icu4c/source && bash -x ../../../build.sh"
 
-#cd icu/icu4c/source && bash -x ../../../build.sh
+if `which emcc`
+then
+  docker rmi trzeci/emscripten
+  docker run --rm -v `pwd`:/src trzeci/emscripten \
+    bash -c "$buildCmd"
+else
+    bash -c "$buildCmd"
+fi
